09ad6381e67c2be7d9539961b07adc7497d93872
Deniz OZOGUL <deniz.ozogul@bilgeadam.com>
2020-11-15 00:19:36 +0300

    blog added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <!-- Header -->
+    <header>
+      <div class="head-top">
+        <a href="#" class="menu-btn"><span></span></a>
+        <div class="top-menu">
+          <ul>
+            <li><a href="https://blog.denizozogul.com" class="lnk">Blog</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
     <div class="wrapper"><router-view /></div>
   </div>
 </template>
