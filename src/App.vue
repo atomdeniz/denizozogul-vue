60c50fca085eeb339178a43af9b0090b8937f4c3
Deniz OZOGUL <deniz.ozogul@bilgeadam.com>
2020-11-14 23:44:28 +0300

    chages

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <template>
   <div id="app">
-    <header>
-      <div class="head-top">
-        <a href="#" class="menu-btn"><span></span></a>
-        <div class="top-menu">
-          <ul>
-            <!-- <li>
-              <router-link to="/" class="btn">Home</router-link>
-            </li> -->
-            <li>
-              <router-link to="/Contact" class="btn">Contact</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </header>
     <div class="wrapper"><router-view /></div>
   </div>
 </template>
