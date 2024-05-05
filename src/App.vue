3166d322116805d40c9ad6a3dac528a3fcbafd3a
Deniz Ozogul <atom_deniz@hotmail.com>
2024-05-05 21:22:31 +0300

    fix favicon

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
     <!-- Header -->
     <header>
       <div class="head-top">
-        <!-- <a href="#" class="menu-btn"><span>Menü</span></a> -->
         <div class="top-menu">
-          <ul>
-            <li><a href="https://blog.denizozogul.com" class="lnk">Blog</a></li>
-          </ul>
         </div>
       </div>
     </header>
